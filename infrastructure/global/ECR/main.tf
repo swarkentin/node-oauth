289436e654edc5861456611b7e1864e78e62b7bf
Sheldon Warkentin <sheldon.warkentin@gmail.com>
2018-11-12 14:26:47 -0800

    Support multiple workspaces
    To create a new workspace (say named 'prod') perform the following:
    
    ```shell
    $ terraform workspace new prod base
    $ terraform workspace select prod base
    
    ```

--- infrastructure/global/ECR/main.tf
+++ infrastructure/global/ECR/main.tf
@@ -1,13 +1,3 @@
-module "backend" {
-  source = "../../modules/backend"
-
-  bootstrap      = "${terraform.workspace == "base" ? 1 : 0}"
-  operators      = "${local.operators}"
-  bucket         = "${var.bucket}"
-  dynamodb_table = "${var.dynamodb_table}"
-  key            = "${var.key}"
-}
-
 provider "aws" {
   profile = "${var.profile}"
   region  = "${var.region}"
--- infrastructure/global/ECR/variables.tf
+++ infrastructure/global/ECR/variables.tf
@@ -2,3 +2,11 @@ variable "ecr-repository-name" {
   description = "The name of the ECS docker repository"
   default = "node-oauth"
 }
+
+variable "profile" {
+  description = "AWS profile"
+}
+
+variable "region" {
+  description = "AWS Region"
+}
